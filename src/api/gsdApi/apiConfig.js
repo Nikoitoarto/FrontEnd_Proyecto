d416714880a98aaff8cdbf034dbea3d838604bb9
Johann Nikolas Ardila Tovar <115807467+Nikoitoarto@users.noreply.github.com>
2024-11-17 00:19:03 -0500

    se actualiza el consumo del token para que cargue dinamicamente

--- src/api/gsdApi/apiConfig.js
+++ src/api/gsdApi/apiConfig.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
 import { getGsdApiToken } from 'utils/storage';
 
-const getHeaders = () => ({
-  'Content-Type': 'application/json',
-  ...(getGsdApiToken() && { 'Authorization': `Bearer ${getGsdApiToken()}` })
-});
-
+// Configurar la instancia de Axios
 const api = axios.create({
   baseURL: 'http://localhost:9080/proyectoformulario/api',
-  timeout: 10000, 
-  headers: getHeaders(),
+  timeout: 10000,
 });
+
+// Añadir un interceptor para incluir el token dinámicamente
+api.interceptors.request.use(
+  (config) => {
+    const token = getGsdApiToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    // Manejar errores en la configuración de la solicitud
+    return Promise.reject(error);
+  }
+);
 
 export default api;
